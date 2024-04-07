36615d6be73c1b9d8bdfcf1dea6453f87fa4f7cc
Procodester <110738169+Procodester@users.noreply.github.com>
2024-04-07 21:03:42 +0530

    add calculator

---
+++ React/Roll Dice/Calculator/src/components/Logo/Logo.jsx
@@ -0,0 +1,16 @@
+// Logo Component
+
+import './logo.css'
+
+function Logo() {
+  return (
+    <div className='logo'>
+      <button className="logo-key logo-key-text">+</button>
+      <button className="logo-key logo-key-text">-</button>
+      <button className="logo-key logo-key-text" >x</button>
+      <button className="logo-key logo-key-text">/</button>
+    </div>
+  )
+}
+
+export default Logo
